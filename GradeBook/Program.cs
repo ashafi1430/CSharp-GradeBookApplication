f7fc50c62339683e794419786b2bc23aa482290c
ashafi1430 <41396981+ashafi1430@users.noreply.github.com>
2018-07-23 22:19:58 +0100

    Added my debug section

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using GradeBook.Enums;
 using GradeBook.UserInterfaces;
 
 namespace GradeBook
@@ -11,6 +13,27 @@ namespace GradeBook
             Console.WriteLine("# Welcome to GradeBook! #");
             Console.WriteLine("#=======================#");
             Console.WriteLine();
+
+            var students = new List<Student>
+            {
+                new Student("jamie",StudentType.Standard,EnrollmentType.Campus)
+                {
+                    Grades = new List<double>{ 100, 102 }
+                },
+                new Student("john",StudentType.Standard,EnrollmentType.Campus)
+                {
+                    Grades = new List<double>{ 75 }
+                }
+            };
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.Name);
+                foreach (var item in student.Grades)
+                {
+                    Console.WriteLine(item);
+                }
+            }
 
             StartingUserInterface.CommandLoop();
             
