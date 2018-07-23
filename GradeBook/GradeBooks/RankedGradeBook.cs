b0dcd852a4b4e8e22ea4cdf695667ff70f24daf4
ashafi1430 <41396981+ashafi1430@users.noreply.github.com>
2018-07-23 21:53:50 +0100

    Step 3.2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,6 +4,11 @@ using System;
 
 namespace GradeBook.GradeBooks
 {
+    /// <summary>
+    /// Information Ranked-grading grades students not based on their individual performance,
+    /// but rather their performance compared to their peers. This means the 20% of the students
+    /// with the highest grade of a class get an A, the next highest 20% get a B, etc.
+    /// </summary>
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
@@ -16,6 +21,34 @@ namespace GradeBook.GradeBooks
             if (Students.Count() < 5)
             {
                 throw new InvalidOperationException("Min is 5 students for Ranked grading.");
+            }
+
+            // from total number of students, return/split 20% of them
+            var groupStudentsNumber = (int)Math.Ceiling(Students.Count * 0.2);
+
+            // Using Ling, sort the list into sorted list.
+            var sortedGrades = Students.OrderBy(students => students.AverageGrade).
+                Select(students => students.AverageGrade).ToList();
+
+            if (sortedGrades[groupStudentsNumber - 1] >= averageGrade)
+            {
+                return 'A';
+            }
+            else if (sortedGrades[(groupStudentsNumber * 2) - 1] >= averageGrade)
+            {
+                return 'B';
+            }
+            else if (sortedGrades[(groupStudentsNumber * 3) - 1] >= averageGrade)
+            {
+                return 'C';
+            }
+            else if (sortedGrades[(groupStudentsNumber * 4) - 1] >= averageGrade)
+            {
+                return 'D';
+            }
+            else if (sortedGrades[(groupStudentsNumber * 5) - 1] >= averageGrade)
+            {
+                return 'E';
             }
 
             return 'F';
